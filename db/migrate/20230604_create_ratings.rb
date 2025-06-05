07f3e8862eacef82ed9cc1f97dc33dd3e2858ea9
Bryan Dailey <bwdailey@gmail.com>
2025-06-05 11:30:28 -0400

    Fix Ruby linting issues

--- db/migrate/20230604_create_ratings.rb
+++ db/migrate/20230604_create_ratings.rb
@@ -2,14 +2,18 @@
 
 class CreateRatings < ActiveRecord::Migration[6.1]
   def change
-    create_table :topic_ratings do |t|
-      t.integer :topic_id, null: false
-      t.integer :user_id, null: false
-      t.string :rating_type, null: false
-      t.integer :value, null: false
-      t.timestamps null: false
-    end
+    safety_assured do
+      create_table :topic_ratings do |t|
+        t.integer :topic_id, null: false
+        t.integer :user_id, null: false
+        t.string :rating_type, null: false
+        t.integer :value, null: false
+        t.timestamps null: false
+      end
 
-    add_index :topic_ratings, [:topic_id, :user_id, :rating_type], unique: true
+      add_index :topic_ratings, %i[topic_id user_id rating_type], unique: true, name: 'idx_topic_ratings_unique'
+      add_foreign_key :topic_ratings, :topics
+      add_foreign_key :topic_ratings, :users
+    end
   end
 end
