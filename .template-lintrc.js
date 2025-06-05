ab8c88a9de512713f2ff29669a813a76cfbbf30b
Bryan Dailey <bwdailey@gmail.com>
2025-06-05 12:21:01 -0400

    Add linting configurations

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
-  extends: "ember-template-lint:recommended",
-  plugins: ["ember-template-lint-plugin-discourse"],
+  extends: 'octane',
   rules: {
-    "no-implicit-this": {
-      allow: ["stars", "sortValue"]
-    },
-    "no-html-comments": true,
-    "no-inline-styles": false,
-    "require-input-label": false
+    'no-implicit-this': true,
+    'no-html-comments': true,
+    'no-inline-styles': false,
+    'require-input-label': false,
+    'no-invalid-interactive': false
   }
 };
