606c580e9c357b686f5951b0e1bb6da7087b4e2e
Bryan Dailey <bwdailey@gmail.com>
2025-06-05 11:06:12 -0400

    Add settings and fix api-initializer

--- assets/javascripts/discourse/api-initializers/ratings-columns.js
+++ assets/javascripts/discourse/api-initializers/ratings-columns.js
@@ -1,4 +1,29 @@
-export default function initializeRatingColumns(api) {
-  api.addDiscoveryQueryParam('importance', { replace: true });
-  api.addDiscoveryQueryParam('feasibility', { replace: true });
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "ratings-columns",
+  initialize() {
+    withPluginApi("0.8.31", api => {
+      const site = api.container.lookup("service:site");
+      
+      if (!site.topic_list_columns) {
+        site.set("topic_list_columns", []);
+      }
+      
+      site.topic_list_columns.pushObjects([
+        {
+          name: "importance",
+          title: "Importance"
+        },
+        {
+          name: "feasibility",
+          title: "Feasibility"
+        },
+        {
+          name: "sort",
+          title: "Sort"
+        }
+      ]);
+    });
+  }
 }
